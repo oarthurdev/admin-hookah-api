48828427a001b0f7d1000c73945cb8bb0c3295d6
Arthur Wagenknecht <oarthurdev@gmail.com>
2021-03-15 20:24:58 -0300

    agr vai

--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,13 @@ const dotenv = require('dotenv').config();
 
 module.exports = {
   development: {
-    client: 'pg',
-    connection: 'postgres://tvlvyzvopblhzl:0405b758acee723819abe7551ab191b22f4e28aa2b752381646b3c71e63d8e4f@ec2-34-195-233-155.compute-1.amazonaws.com:5432/d9mtlc60qsnv11',
+    client: process.env.DB_CLIENT,
+    connection: {
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        server: process.env.DB_SERVER,
+        database: process.env.DB_NAME
+    },
     migrations: {
       directory: './src/database/migrations'
     },
@@ -14,13 +19,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: {
-      port: process.env.DB_PORT,
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-    },
+    connection: 'postgres://tvlvyzvopblhzl:0405b758acee723819abe7551ab191b22f4e28aa2b752381646b3c71e63d8e4f@ec2-34-195-233-155.compute-1.amazonaws.com:5432/d9mtlc60qsnv11',
     pool: {
       min: 2,
       max: 10
