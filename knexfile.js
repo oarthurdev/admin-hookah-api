6a8775e5774c5731c90738a71713ec77f228bba3
Arthur Wagenknecht <oarthurdev@gmail.com>
2021-03-15 20:23:08 -0300

    update info db

--- knexfile.js
+++ knexfile.js
@@ -2,13 +2,8 @@ const dotenv = require('dotenv').config();
 
 module.exports = {
   development: {
-    client: 'process.env.DB_CLIENT',
-    connection: {
-        user: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        server: process.env.DB_SERVER,
-        database: process.env.DB_NAME
-    },
+    client: 'pg',
+    connection: 'postgres://tvlvyzvopblhzl:0405b758acee723819abe7551ab191b22f4e28aa2b752381646b3c71e63d8e4f@ec2-34-195-233-155.compute-1.amazonaws.com:5432/d9mtlc60qsnv11',
     migrations: {
       directory: './src/database/migrations'
     },
