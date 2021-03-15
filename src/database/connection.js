139742ffa272337f704a03520c56ea9db5602e8d
Arthur Wagenknecht <oarthurdev@gmail.com>
2021-03-15 20:19:31 -0300

    update

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,13 +3,7 @@ const dotenv = require('dotenv').config();
 
 const configurationJSON = {
     client: 'pg',
-    connection: {
-        user: 'tvlvyzvopblhzl',
-        password: '0405b758acee723819abe7551ab191b22f4e28aa2b752381646b3c71e63d8e4f',
-        host: 'postgres://tvlvyzvopblhzl:0405b758acee723819abe7551ab191b22f4e28aa2b752381646b3c71e63d8e4f@ec2-34-195-233-155.compute-1.amazonaws.com:5432/d9mtlc60qsnv11',
-        database: 'd9mtlc60qsnv11',
-        port: 5432
-    },
+    connection: 'postgres://tvlvyzvopblhzl:0405b758acee723819abe7551ab191b22f4e28aa2b752381646b3c71e63d8e4f@ec2-34-195-233-155.compute-1.amazonaws.com:5432/d9mtlc60qsnv11',
     migrations: {
         directory: './src/database/migrations'
     },
