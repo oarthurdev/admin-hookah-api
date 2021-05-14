085d734bc38897fe15eae49113363140e92c06d3
Arthur Wagenknecht <oarthurdev@gmail.com>
2021-05-13 21:57:12 -0300

    update production config

--- knexfile.js
+++ knexfile.js
@@ -19,12 +19,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: {
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      server: process.env.DB_SERVER,
-      database: process.env.DB_NAME
-    },
+    connection: 'postgres://ggbjuckzqldsak:02fcc6e22ab7d0676170f08a9a1e0a6d820926e6966fbdc10a0da68ae9e104c5@ec2-54-164-22-242.compute-1.amazonaws.com:5432/dbh1n0diiu24lq',
     pool: {
       min: 2,
       max: 10
